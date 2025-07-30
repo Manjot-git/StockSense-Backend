62aace163ee471473bba268b5fb24d84065137a2
Manjot-git <manjott13k@gmail.com>
2025-07-30 10:07:57 +0530

    connection b/w mongo-express

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 8080;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("App started!");
+  mongoose.connect(uri);
+  console.log("DB connected successfully!");
 });
